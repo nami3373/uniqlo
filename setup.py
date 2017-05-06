32d94f201ff64c1d6a4a887065a059d505e4a1b2
nami3373 <youhei.sazanami@gmail.com>
2017-05-06 09:22:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,12 @@ df = pd.read_csv('train_master.tsv', delimiter='\t')
 # 色ごとのディレクトリを作成
 color_id = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
 
+for name in color_id:
+	os.mkdir(os.path.join(TRAIN_DIR, name))
+
+for name in color_id:
+	os.mkdir(os.path.join(VALID_DIR, name))
+
 for f in sorted(os.listdir(IN_DIR)):
 	if not f.startswith('.') and os.path.isfile(os.path.join(IN_DIR, f)):
 		i = df[df['file_name'] == f].ix[:, 1].values
